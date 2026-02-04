4e2920a068561dfa567f01821b21e97ddaefb72d
omatheus-edev <mc2019750@gmail.com>
2026-02-04 10:57:01 -0300

    adding support of json array

--- commons/src/main/java/codes/matheus/util/Json.java
+++ commons/src/main/java/codes/matheus/util/Json.java
@@ -2,12 +2,16 @@ package codes.matheus.util;
 
 import codes.matheus.entity.User;
 import codes.matheus.entity.Username;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.internal.bind.JsonTreeWriter;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Json {
     private Json() {
@@ -27,11 +31,37 @@ public final class Json {
         }
     }
 
-    public static @NotNull User deserialize(@NotNull String json) {
+    public static @NotNull JsonArray serialize(@NotNull List<User> users) {
+        try (@NotNull JsonTreeWriter writer = new JsonTreeWriter()) {
+            writer.beginArray();
+            for (@NotNull User user : users) {
+                writer.beginObject();
+                writer.name("id").value(user.getId());
+                writer.name("name").value(user.getUsername().getName());
+                writer.name("email").value(user.getEmail());
+                writer.endObject();
+            }
+            writer.endArray();
+            return writer.get().getAsJsonArray();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to serialize: " + e.getMessage());
+        }
+    }
+
+    public static @NotNull User deserializeObject(@NotNull String json) {
         @NotNull JsonObject object = JsonParser.parseString(json).getAsJsonObject();
         long id = object.get("id").getAsLong();
         @NotNull Username username = new Username(object.get("name").getAsString());
         @NotNull String email = object.get("email").getAsString();
         return new User(id, username, email);
+    }
+
+    public static @NotNull List<User> deserializeArray(@NotNull String json) {
+        @NotNull JsonArray array = JsonParser.parseString(json).getAsJsonArray();
+        @NotNull List<User> users = new ArrayList<>();
+        for (@NotNull JsonElement element : array) {
+            users.add(deserializeObject(element.toString()));
+        }
+        return users;
     }
 }
--- commons/src/test/java/codes/matheus/util/JsonTest.java
+++ commons/src/test/java/codes/matheus/util/JsonTest.java
@@ -2,15 +2,19 @@ package codes.matheus.util;
 
 import codes.matheus.entity.User;
 import codes.matheus.entity.Username;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public final class JsonTest {
     @Test
-    void testSerialize() {
+    void testSerializeObject() {
         @NotNull User user = new User(new Username("Matheus"), "mc2019750@gmail.com");
         @NotNull JsonObject object = Json.serialize(user);
 
@@ -20,10 +24,30 @@ public final class JsonTest {
     }
 
     @Test
+    void testSerializeArray() {
+        @NotNull User user1 = new User(new Username("Carlos"), "sla@email.com");
+        @NotNull User user2 = new User(new Username("junior"), "jr@email.com");
+        @NotNull User user3 = new User(new Username("lucas"), "lc@email.com");
+        @NotNull User user4 = new User(new Username("henrique"), "hrq@email.com");
+        @NotNull List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+
+        @NotNull JsonArray array = Json.serialize(users);
+        @NotNull List<User> copy = Json.deserializeArray(array.toString());
+
+        assertEquals(copy, users);
+        System.out.println(users);
+        System.out.println(copy);
+    }
+
+    @Test
     void testDeserialize() {
         @NotNull User user = new User(new Username("Matheus"), "mc2019750@gmail.com");
         @NotNull JsonObject object = Json.serialize(user);
-        @NotNull User copy = Json.deserialize(object.toString());
+        @NotNull User copy = Json.deserializeObject(object.toString());
 
         assertEquals(copy, user);
         assertEquals(1, copy.getId());
